c7f3fa4a5adbb79a5b4c806e1f5462ef819a264f
Mark McLaren <mark.mclaren@mclarencollege.in>
2019-11-19 02:59:11 +0000

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:day_33_morning_challenge/day_33_morning_challenge.dart' as day_33_morning_challenge;
+
+main(List<String> arguments) {
+  print('Hello world: ${day_33_morning_challenge.calculate()}!');
+}
---
+++ test/day_33_morning_challenge_test.dart
@@ -0,0 +1,9 @@
+import 'package:day_33_morning_challenge/day_33_morning_challenge.dart';
+import 'package:flutter/material.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
